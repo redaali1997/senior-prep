b79b979b492ab7049b465954b624adf4c8894c5f
Reda Ali <reda5201028@gmail.com>
2026-06-30 23:35:09 +0300

    Add simulated runtime failure for rollback testing in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(app()->isProduction()) {
+            throw new \RuntimeException('Simulated runtime failure for rollback test');
+        }
     }
 }
